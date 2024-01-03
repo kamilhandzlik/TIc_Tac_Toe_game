8abecef6ed2fcc865780482e4b4ddb6bace57add
Kamil Handzlik <kamol0209@gmail.com>
2024-01-03 19:04:03 +0100

    set initial files loop and one class upon wich will be build entire image of game

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 from utils import *
+from utils.settings import *
+
+WIN = pg.display.set_mode((WIDTH, HEIGHT))
+pg.display.set_caption("TIc Tac Toe game")
+
+
+run = True
+clock = pg.time.Clock()
+
+while run:
+    clock.tick(FPS)
+
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            run = False
+
+pg.quit()
