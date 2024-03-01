e7edc8b00f80507dd7e66704e944202ee9380764
Kamil Handzlik <kamol0209@gmail.com>
2024-03-01 20:24:21 +0100

    added new atributes to comp_move function method clicked and created winner_list function winner, check_winner

--- main.py
+++ main.py
@@ -23,7 +23,8 @@ class Square(pg.sprite.Sprite):
         self.rect.topleft = (self.x, self.y)
 
     def clicked(self, x_value, y_value):
-        global turn
+        global turn, won
+
         if self.content == "":
             if self.rect.collidepoint(x_value, y_value):
                 self.content = turn
@@ -35,7 +36,10 @@ class Square(pg.sprite.Sprite):
                         self.image, (self.width, self.height)
                     )
                     turn = "o"
-                    comp_move()
+                    check_winner("x")
+
+                    if not won:
+                        comp_move()
 
                 else:
                     self.image = o_image
@@ -43,6 +47,56 @@ class Square(pg.sprite.Sprite):
                         self.image, (self.width, self.height)
                     )
                     turn = "x"
+                    check_winner("o")
+
+
+def check_winner(player):
+    global background, won
+
+    for i in range(8):
+        if (
+            board[winning_list[i][0]] == player
+            and board[winning_list[i][1]] == player
+            and board[winning_list[i][2]] == player
+        ):
+            won = True
+            break
+
+    if won:
+        Update()
+        square_group.empty()
+        background = pg.image.load(player.upper() + "Wins.png")
+        background = pg.transform.scale(background, (WIDTH, HEIGHT))
+
+
+def winner(player):
+    global comp_move, move
+
+    for i in range(8):
+        if (
+            board[winning_list[i][0]] == player
+            and board[winning_list[i][1]] == player
+            and board[winning_list[i][2]] == ""
+        ):
+            comp_move = winning_list[i][2]
+            move = False
+            break
+
+        elif (
+            board[winning_list[i][0]] == player
+            and board[winning_list[i][1]] == ""
+            and board[winning_list[i][2]] == player
+        ):
+            comp_move = winning_list[i][1]
+            move = False
+
+        elif (
+            board[winning_list[i][0]] == ""
+            and board[winning_list[i][1]] == player
+            and board[winning_list[i][2]] == player
+        ):
+            comp_move = winning_list[i][0]
+            move = False
 
 
 def comp_move():
@@ -50,17 +104,23 @@ def comp_move():
     move = True
 
     if move:
+        winner("o")
+
+    if move:
+        winner("x")
+
+    if move:
         check_center()
 
     if move:
-        check_corner
+        check_corner()
 
     if move:
         check_edge()
 
     if not move:
         for square in squares:
-            if square.number == comp_move:
+            if square.number == compmove:
                 square.clicked(square.x, square.y)
 
 
@@ -112,28 +172,38 @@ pg.display.set_caption("Tic Tac Toe game")
 blank_image = pg.image.load("Blank.png")
 x_image = pg.image.load("x.png")
 o_image = pg.image.load("o.png")
-x_wins_image = pg.image.load("X Wins.png")
-o_wins_image = pg.image.load("O Wins.png")
-BG_image = pg.image.load("Background.png")
-tie_image = pg.image.load("Tie Game.png")
+x_wins_image = pg.image.load("x wins.png")
+o_wins_image = pg.image.load("o wins.png")
+BG_image = pg.image.load("background.png")
+tie_image = pg.image.load("tie game.png")
 background = pg.transform.scale(BG_image, (WIDTH, HEIGHT))
-
-move = True
-compmove = 5
-
-
-def get_font():
-    return pg.font.SysFont("Calibri", 20)
 
 
 # main
+move = True
+won = False
+compmove = 5
 
 square_group = pg.sprite.Group()
 squares = []
 turn = "x"
-board = ["" for i in range(10)]
+winning_list = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7],
+]
 
+board = ["" for i in range(10)]
+run = True
+clock = pg.time.Clock()
 num = 1
+
+
 for y in range(1, 4):
     for x in range(1, 4):
         sq = Square(x, y, num)
@@ -142,19 +212,15 @@ for y in range(1, 4):
         num += 1
 
 
-run = True
-clock = pg.time.Clock()
-
 while run:
     clock.tick(FPS)
     for event in pg.event.get():
         if event.type == pg.QUIT:
             run = False
+
         if event.type == pg.MOUSEBUTTONDOWN and turn == "x":
             mx, my = pg.mouse.get_pos()
             for s in squares:
                 s.clicked(mx, my)
 
     Update()
-
-pg.quit()
